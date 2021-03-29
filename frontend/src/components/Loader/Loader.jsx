7f0743f609a4323c8e154200b897cc043849eef2
unknown <arshia_nazarlotfi1376@yahoo.com>
2021-03-29 17:36:36 +0430

    adding margin top to loader

--- frontend/src/components/Loader/Loader.jsx
+++ frontend/src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import Gif from './Loader.gif';
 
 const Loader = () => {
   return (
-    <div style={{ textAlign: 'center', width: '100%', margin: '1rem 0' }}>
+    <div style={{ textAlign: 'center', width: '100%', margin: '5rem 0' }}>
       <img
         style={{ width: '10rem', height: '10rem' }}
         src={Gif}
