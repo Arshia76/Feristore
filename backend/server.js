d027ead11acbd11504db350a3383633b3933ee1a
unknown <arshia_nazarlotfi1376@yahoo.com>
2021-03-30 00:12:04 +0430

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,26 @@ app.use('/api/categories', require('./routes/category'));
 app.use('/api/reviews', require('./routes/review'));
 app.use('/api/orders', require('./routes/orders'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
+  app.use(express.static(path.join(path.resolve(), '/frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(path.resolve(), 'frontend', 'build', 'index.html')
+    );
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Api is running');
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
