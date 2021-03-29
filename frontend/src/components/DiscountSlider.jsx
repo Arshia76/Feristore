3f1b5a3c3be6b246111e475af4232b2e47e0c626
unknown <arshia_nazarlotfi1376@yahoo.com>
2021-03-29 17:35:56 +0430

    changing react-slick to owl carousel

--- frontend/src/components/DiscountSlider.jsx
+++ frontend/src/components/DiscountSlider.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
-import Slider from 'react-slick';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 import {
   Typography,
   Card,
@@ -50,90 +53,75 @@ const DiscountSlider = () => {
   const history = useHistory();
   const productContext = useContext(ProductContext);
 
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: false,
-          arrows: true,
-          dots: true,
-          initialSlide: 0,
-        },
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 0,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
-    <Box style={{ padding: '1rem 0', margin: '2rem 0', textAlign: 'center' }}>
-      {productContext.discountedProducts.length > 0 && (
-        <Fragment>
-          <Typography className={classes.text} variant='h3'>
-            محصولات دارای تخفیف
-          </Typography>
-          <Slider style={{ textAlign: 'right' }} {...settings}>
-            {productContext.loading ? (
-              <Loader />
-            ) : (
-              productContext.discountedProducts.map((product) => {
-                return (
-                  <Card className={classes.card} key={product._id}>
-                    <CardActionArea>
-                      <CardMedia
-                        component='img'
-                        className={classes.image}
-                        src={product.image}
-                        onClick={async () => {
-                          await productContext.productDetail(product._id);
-                          history.push(`/detail/${product._id}`);
-                        }}
-                      />
-                      <CardContent className={classes.CardContent}>
-                        <Typography gutterBottom variant='h5' component='h6'>
-                          {product.name}
-                        </Typography>
-                        <Typography gutterBottom variant='h6' component='h6'>
-                          {`${product.discount}%`}
-                        </Typography>
-
-                        <Box className={classes.price}>
-                          <Typography style={{ marginLeft: '.3rem' }}>
-                            {product.price}
+    <Fragment>
+      {productContext.loading ? (
+        <Loader />
+      ) : (
+        productContext.discountedProducts.length > 0 && (
+          <Box
+            style={{ padding: '1rem 0', margin: '2rem 0', textAlign: 'center' }}
+          >
+            <Fragment>
+              <Typography className={classes.text} variant='h3'>
+                محصولات دارای تخفیف
+              </Typography>
+              <OwlCarousel
+                style={{ textAlign: 'right' }}
+                dots={true}
+                nav={true}
+                responsive={{
+                  500: {
+                    items: 1,
+                  },
+                  600: {
+                    items: 2,
+                  },
+                  1024: {
+                    items: 3,
+                  },
+                }}
+                margin={15}
+                className='owl-theme'
+              >
+                {productContext.discountedProducts.map((product) => {
+                  return (
+                    <Card className={classes.card} key={product._id}>
+                      <CardActionArea>
+                        <CardMedia
+                          component='img'
+                          className={classes.image}
+                          src={product.image}
+                          onClick={async () => {
+                            await productContext.productDetail(product._id);
+                            history.push(`/detail/${product._id}`);
+                          }}
+                        />
+                        <CardContent className={classes.CardContent}>
+                          <Typography gutterBottom variant='h5' component='h6'>
+                            {product.name}
                           </Typography>
-                          <Typography>تومان</Typography>
-                        </Box>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                );
-              })
-            )}
-          </Slider>
-        </Fragment>
+                          <Typography gutterBottom variant='h6' component='h6'>
+                            {`${product.discount}%`}
+                          </Typography>
+
+                          <Box className={classes.price}>
+                            <Typography style={{ marginLeft: '.3rem' }}>
+                              {product.price}
+                            </Typography>
+                            <Typography>تومان</Typography>
+                          </Box>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  );
+                })}
+              </OwlCarousel>
+            </Fragment>
+          </Box>
+        )
       )}
-    </Box>
+    </Fragment>
   );
 };
 
